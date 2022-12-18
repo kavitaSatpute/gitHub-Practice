7b263863d6dbe398cf449fb4b372754986d0bdde
kavita <kavitabhandekar17@gmail.com>
2022-12-17 16:57:20 -0800

    New Changes

--- src/main/java/NeoStoxBase/Base_NeoStox.java
+++ src/main/java/NeoStoxBase/Base_NeoStox.java
@@ -23,6 +23,7 @@ public class Base_NeoStox {
    }
    
    
+   
    public void closingBrowser(WebDriver driver)
    {   utilityNeostoxusingPropertyfile.wait(driver, 1000);
        Reporter.log("Closing Browser...",true);
